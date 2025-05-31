8408fca7cb9f9d30b61836c423a23dafb260bfef
samidevrimoyar <sami.devrim@gmail.com>
2025-05-31 20:17:42 +0300

    Ogame için static tanımı eklendi

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -16,6 +16,8 @@ import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
