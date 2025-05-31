c9bc20ce80fe3498a4fcd3977c5a6cd08dae8802
samidevrimoyar <sami.devrim@gmail.com>
2025-05-31 21:01:52 +0300

    syntax error düzeltildi

--- profiles_project/settings.py
+++ profiles_project/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'profiles_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [], [os.path.join(BASE_DIR, 'static')],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
